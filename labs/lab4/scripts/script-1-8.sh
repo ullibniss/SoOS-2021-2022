f43401fb91a1d2d72cab638e0944f2402c086ede
Alexey Fedorov <89032904807alek@gmail.com>
2021-09-09 01:47:01 -0400

    1.8 - process count

--- labs/lab4/scripts/script-1-8.sh
+++ labs/lab4/scripts/script-1-8.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-
-ps aux | awk '$1="alexey"{print $0}'
+WHOAMI=$(whoami)
+echo "--- $WHOAMI processes amout ---"
+ps aux | awk '{
+if ($1==ENVIRON["LOGNAME"])
+	{print $0}
+}' | wc -l
+echo   ""
+echo "--- root processes amount ---"
+ps aux | awk '{
+if ($1=="root")
+	{print $0}
+}' | wc -l
