9d8e8adf8806c53745a5c42475b14fa66209599f
Alexey Fedorov <89032904807alek@gmail.com>
2021-09-11 16:22:59 -0400

    bash.txt zeros/nonzeros

---
+++ labs/lab4/scripts/script-2-3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+od bash.txt | grep 000000 > /tmp/zeros
+od bash.txt | grep -v 000000 > /tmp/nonzeros
+
+echo "-----Zero-----"
+head -10 /tmp/zeros
+echo "--------------"
+tail -10 /tmp/zeros
+echo "-----Nonzero-----"
+head -10 /tmp/nonzeros
+echo "-----------------"
+tail -10 /tmp/nonzeros
