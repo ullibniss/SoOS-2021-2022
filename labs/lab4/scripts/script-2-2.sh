8c19e1f597884597e1bf3cd4ac793a5542287e89
Alexey Fedorov <89032904807alek@gmail.com>
2021-09-11 16:22:27 -0400

    exec-less

---
+++ labs/lab4/scripts/script-2-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+~/SoOS-2021-2022/labs/lab4/scripts/script-2-1.sh
+
+#2nd case - Same folder
+#./script-2-1.sh
+
+#3rd case - Symlink
+#ln -s ./script-2-1.sh ~/bin/script-2-1.sh
+#script-2-1.sh
+
+echo "---------------"
+less /tmp/run.log
