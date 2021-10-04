21a13900df8b942de1032310d165df01e19a2a53
Alexey <55274498+ullibniss@users.noreply.github.com>
2021-10-04 16:18:00 +0300

    fixed 2.2 - exec/less

--- labs/lab4/scripts/script-2-2.sh
+++ labs/lab4/scripts/script-2-2.sh
@@ -5,8 +5,8 @@
 #2nd case - Same folder
 #./script-2-1.sh
 
-#3rd case - Symlink
-#ln -s ./script-2-1.sh ~/bin/script-2-1.sh
+#3rd case - Hardlink
+#ln ./script-2-1.sh ~/bin/script-2-1.sh
 #script-2-1.sh
 
 echo "---------------"
