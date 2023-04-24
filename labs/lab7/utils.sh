80c08e3782dc71b1f34d981f6af5e0b12d9ec547
Alexey <89032904807alek@gmail.com>
2023-04-24 06:51:03 +0300

    Refactored column menu #

--- labs/lab7/utils.sh
+++ labs/lab7/utils.sh
@@ -47,10 +47,13 @@ function column_menu()
     COLUMN_MENU_NUMLIST=""
     for i in $COLUMN_MENU_LIST; do
         echo $(( COLUMN_MENU_COUNTER++ )) &> /dev/null
-        COLUMN_MENU_NUMLIST="$COLUMN_MENU_NUMLIST"$'\n'"${COLUMN_MENU_COUNTER})${i}"
+        COLUMN_MENU_NUMLIST="$COLUMN_MENU_NUMLIST*${COLUMN_MENU_COUNTER})${i}"
+        if (( $COLUMN_MENU_COUNTER % 4 == 0 )); then
+            COLUMN_MENU_NUMLIST="$COLUMN_MENU_NUMLIST"$'\n'
+        fi
     done
 
-    column -s " " < <(echo "$COLUMN_MENU_NUMLIST") | prSection $(( $(echo "$COLUMN_MENU_LIST" | wc -l) / 3 + 1)) 
+    column -s '*' -t < <(echo "$COLUMN_MENU_NUMLIST") | prSection $(( $(echo "$COLUMN_MENU_LIST" | wc -l) / 3 + 1)) 
 
     echo -n "$COLUMN_MENU_CHOOSE_PHRASE" 
     read column_menu_choose
